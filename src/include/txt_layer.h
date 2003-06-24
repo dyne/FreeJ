b194182298b0c1b14dab227155dc49e01b788a87
kysucix <kysucix@383723c8-4afa-0310-b8a8-b1afb83214fc>
2003-06-24 20:39:52 +0000

    new rendering,advancing with kerning,blinking
    git-svn-id: svn://dyne.org/rastasoft/freej/freej@72 383723c8-4afa-0310-b8a8-b1afb83214fc

--- src/include/txt_layer.h
+++ src/include/txt_layer.h
@@ -23,21 +23,47 @@
 #include <layer.h>
 
 #include <ft2build.h>
-#include FT_FREETYPE_H
+#include FT_FREETYPE_H /* bha' */
+#include FT_GLYPH_H /* wtf? */
+
 #include <ctype.h>
 #include <SDL/SDL.h>
+#define MAX_GLYPHS 512
+typedef struct TGlyph_
+{
+     FT_UInt    glyph_index;    // glyph index
+     FT_Vector  baseline_position;      // glyph origin on the baseline
+     FT_Glyph   image;    // glyph image
+
+} TGlyph, *PGlyph;
 
 class TxtLayer: public Layer {
  private:
   int fd;
   
   /* handle to library     */
-  FT_Library   library;   
+  FT_Library library;   
   
   /* handle to face object */
-  FT_Face      face;      
-  FT_GlyphSlot  slot;
+  FT_Face face;      
+
+  /* handle to glyph image */
+  FT_Glyph glyph;
+
+  /* Transformation matrix for FT_Glyph_Transform()*/
+  FT_Matrix matrix; /* neo sux */
   
+  /* Translation vector for FT_Glyph_To_Bitmap() */
+  FT_UInt num_glyphs;
+  int x,y;
+
+  /* glyphs table */
+  TGlyph glyphs[ MAX_GLYPHS ]; 
+
+  /* current glyph in table */
+  PGlyph glyph_current; 
+  FT_UInt glyphs_numbers;
+
   char line[512]; // safe bound
   int line_len;
   char *word, *punt;
@@ -45,25 +71,28 @@ class TxtLayer: public Layer {
 
   bool change_word;
   bool clear_screen;
-  int text_dimension, ascissa, letter;
+  bool blinking;
+  int text_dimension;
   
   /* image buffer */
   void *buf;
-  int center;
   
   int word_ff(int pos);
+  bool draw_character(FT_BitmapGlyph bitmap, int left_side_bearing, int top_side_bearing,Uint8 *dst);
+  bool set_character_size(int _text_dimension);
   //  int word_rw(int pos);
 
 public:
   TxtLayer();
   ~TxtLayer();
   
-  bool init(Context *screen=NULL);
+  bool init(Context *screen=NULL,int _text_dimension=64);
   bool open(char *file);
   void *feed();
   void *get_buffer();
   void close();
   bool keypress(SDL_keysym *keysym);
+  void compute_string_bbox( FT_BBox  *abbox,FT_Glyph image );
 };
 
 #endif
--- src/txt_layer.cpp
+++ src/txt_layer.cpp
@@ -32,208 +32,361 @@
 
 
 TxtLayer::TxtLayer()
-  :Layer() {
-  buf = NULL;
-  setname("TXT");
-  change_word=true;
-  text_dimension=0;
-  clear_screen=false;
-}
+     :Layer() {
+	  buf = NULL;
+	  setname("TXT");
+	  change_word=true;
+	  clear_screen=false;
+	  blinking=false;
+	  text_dimension=0;
+	  glyph_current=NULL;
+	  x=0;
+	  y=0;
+     }
 
 TxtLayer::~TxtLayer() {
-  close();
+     close();
 }
 
 bool TxtLayer::open(char *file) {
-  func("TxtLayer::open(%s)",file);
-  
-  fd = ::open(file,O_RDONLY|O_NONBLOCK);
-  if(fd<0) {
-    error("TxtLayer::open(%s) - %s",
-	  file, strerror(errno));
-    return (false);
-  }
-  
-  set_filename(file);
-  
-  return(true);
+     func("TxtLayer::open(%s)",file);
+
+     fd = ::open(file,O_RDONLY|O_NONBLOCK);
+     if(fd<0) {
+	  error("TxtLayer::open(%s) - %s",
+		    file, strerror(errno));
+	  return (false);
+     }
+
+     set_filename(file);
+
+     return(true);
 }
 
-bool TxtLayer::init(Context *scr) {
+bool TxtLayer::init(Context *scr,int _text_dimension) {
 
-  text_dimension=64;
-  
-  /* Initialize the freetype library */
-  if(FT_Init_FreeType( &library )) {
-    error("TxtLayer::Couldn't initialize freetype" );
-    return(false);
-  }
-  if(FT_New_Face( library, "/usr/X11R6/lib/X11/fonts/truetype/arial.ttf", 0, &face ))  {
-    error("TxtLayer::Couldn't load ttf font");
-    return(false);
-  }
-  
-  /* Guess?? */
-  FT_Set_Pixel_Sizes(face, 0, text_dimension);
-     
-  /* Comfortable */
-  slot = face->glyph; 
-  
-  if(scr) screen = scr;
-  _init(screen, screen->w, screen->h, screen->bpp);
-  
-  buf = jalloc(buf,screen->size);
+     if(_text_dimension < 0) {
+	  error("TxtLayer::init() - Character size must be positive dude!");
+	  return(false);
+     }
+     text_dimension=_text_dimension;
 
-  /* get the first word */
-  line_len = 0;  
-  punt = word = line;
-  
-  /* TODO: fetch some more data about the chars and use it
-     to center them better */
-  center = (int)(geo.pitch*(geo.h/2.5)) + geo.pitch/2;
-  return(true);
+     /* Initialize the freetype library */
+     if(FT_Init_FreeType( &library )) {
+	  error("TxtLayer::Couldn't initialize freetype" );
+	  return(false);
+     }
+
+     /* Create face object */
+     if(FT_New_Face( library, "/usr/X11R6/lib/X11/fonts/truetype/comic.ttf", 0, &face ))  {
+	  error("TxtLayer::Couldn't load ttf font");
+	  return(false);
+     }
+
+     /* Set Character size */
+     if(!set_character_size(text_dimension))
+	  return(false);
+
+     if(scr) screen = scr;
+     _init(screen, screen->w, screen->h, screen->bpp);
+
+     buf = jalloc(buf,screen->size);
+
+     /* get the first word */
+     line_len = 0;  
+     punt = word = line;
+
+     x=(geo.w/2)*(geo.bpp/8);
+     y=geo.h/2;
+     return(true);
 }
 
 void *TxtLayer::feed() {
-  int ret,z,i,c;
-  
-  /* One word */
-  if(change_word && !clear_screen) {
-    int n=0;
-    word_len = word_ff(1);
-    /* load glyph image into the slot */
-    ret = FT_Load_Char( face, word[n], FT_LOAD_RENDER );
-    letter=0;
-    
+     int ret;
+     int origin_x=0;
+     int origin_y=0;
+     int previous=0;
+     int string_width,string_height;
+     //int angle=30;
+     FT_Bool use_kerning=FT_HAS_KERNING(face);
+     FT_BBox string_bbox;
+     FT_Vector vector;
 
-    /* first inner loop:
-       draw the whole word */
-    for(i=0 ; i<word_len ; i++) {
-      
-      Uint8 *src = slot->bitmap.buffer ;
-      Uint8 *dst= (Uint8 *) buf;
-      
-      /* optimized to not use any multiply or division
-	 === now ascissa became word_len (which is a more explicative variable name)
-	 ascissa was lately used in the inner loop as
-	 (ascissa/2)*text_dimension
-	 so i made text_dimension a power of 2 (128) and it comes
-	 (ascissa>>1)<<7 which can be semplified to a shift left of 6 bits
-	 [jrml 19may03] */
-      dst += center + (letter << 5) - (word_len<<5);
-      /* dirty fix for the lineup of the 'l' and some other letters 
-	 TODO : implement a decent alignement method for letters */
-      if( word[n] == 'l'
-	  || word[n] == 't'
-	  || word[n] == 'f'
-	  || word[n] == 'i'
-	  || word[n] == 'd'
-	  || word[n] == 'b'
-	  || word[n] == 'I'
-	  || word[n] == 'h')
-	dst -= (geo.pitch<<3) + (geo.pitch<<2);
+     /* One word  main loop, one word each iteration*/
+     if(change_word && !clear_screen) {
+	  glyph_current=glyphs;
+	  //printf("WORD: %s\n",word);
+	  word_len = word_ff(1);
+
+	  /*  Convert the character string into a series of glyph indices. */
+	  for ( int n = 0; n < word_len; n++ ) {
+	       glyph_current->glyph_index = FT_Get_Char_Index( face, word[n] );
+	       if ( use_kerning && previous && glyph_current->glyph_index ) {
+		    FT_Vector kerning_vector;
+		    FT_Get_Kerning( face, previous, glyph_current->glyph_index, ft_kerning_unfitted, &kerning_vector );
+
+		    /*  Place the pen to the cursor position. */
+		    origin_x += kerning_vector.x;
+		    origin_y = 0;
+	       }
+	       /* store current pen position */
+	       glyph_current->baseline_position.x = origin_x;
+	       glyph_current->baseline_position.y = origin_y;
+
+	       /* load the glyph image (in its native format); */
+	       ret = FT_Load_Glyph( face, glyph_current->glyph_index, FT_LOAD_DEFAULT );
+	       if (ret) continue; // ignore errors, jump to next glyph */
+
+	       /* extract glyph image and store it in our table */
+	       ret = FT_Get_Glyph( face->glyph, &glyph_current->image );
+	       if (ret) continue;
+
+	       /* increment pen position */
+	       origin_x += face->glyph->advance.x;
+
+	       /* record current glyph index in previous (4 kerning) */
+	       previous = glyph_current->glyph_index;
+
+	       /* increment the current pointer*/
+	       glyph_current++;
+	  }
+	  /* count number of glyphs loaded.. */
+	  num_glyphs = glyph_current - glyphs;
+
+	  /* get bbox of original glyph sequence */
+	  compute_string_bbox( &string_bbox, glyph_current->image);
+
+	  /* compute string dimensions in integer pixels */
+	  string_width  = (string_bbox.xMax - string_bbox.xMin)<<6;
+	  string_height = (string_bbox.yMax - string_bbox.yMin)<<6;
+	  printf("string_width: %d\n string_height: %d",string_width,string_height);
+
+	  /* set up position in 26.6 cartesian space */
+
+	  vector.x = (x<<6)-(( origin_x / 2 ) & -64);
+	  vector.y = (geo.h-y)<<6;
 
 
-      /* Second inner loop: draw a letter;
-	 they come around but they never come close to 
-	 TODO: this has to be fixed indeed */
-      for(z=0 ; z<(slot->bitmap.rows) ; z++) {
+	  /* set up transform (a rotation here) */
+	  //matrix.xx = (FT_Fixed)( cos(angle)*0x10000L);
+	  //matrix.xy = (FT_Fixed)(-sin(angle)*0x10000L);
+	  //matrix.yx = (FT_Fixed)( sin(angle)*0x10000L);
+	  //matrix.yy = (FT_Fixed)( cos(angle)*0x10000L);
 
-	for(c = slot->bitmap.width; c>0 ; c--) {
-	  //	  *dst++ = *src;
-	  *dst++ = *src++;
-	  // dst[1] = *src;
-	  // dst[2] = *src;
-	  // dst[3] = (alpha_blit) ? 0x00 : 0xff;
-	}
-	// memcpy( dst, src, slot->bitmap.pitch );
-	// src += slot->bitmap.pitch;
-	dst += geo.pitch - slot->bitmap.pitch;
-      }
-      
-      n++;
-      letter+=2;
-      ret = FT_Load_Char( face, word[n], FT_LOAD_RENDER );
-    }
-    
-    change_word=false;
-  }
-  /* now guess */
-  else if(clear_screen) {
-    bzero( buf, geo.size);
-    clear_screen=false;
-  }
-  
-  return buf;
+	  for ( unsigned int m = 0; m < num_glyphs; m++ )
+	  {
+	       FT_Glyph  image;
+	       FT_BBox   bbox;
+	       FT_Vector translate_vector;
+
+	       translate_vector=glyphs[m].baseline_position;
+	       translate_vector.x+=vector.x;
+	       translate_vector.y+=vector.y;
+
+	       /* create a copy of the original glyph */
+	       ret = FT_Glyph_Copy( glyphs[m].image, &image );
+	       if (ret)  {
+		    error("TxtLayer::FT_Glyph_Copy()");
+		    continue;
+	       }
+	       /* transform copy (this will also translate it to the correct position */
+	       FT_Glyph_Transform( image, 0, &translate_vector );
+
+	       /* check bounding box, if the transformed glyph image 
+		* is not in our target surface, we can avoid rendering it 
+		*/
+	       FT_Glyph_Get_CBox( image, ft_glyph_bbox_pixels, &bbox );
+	       if ( bbox.xMax <= 0 || bbox.xMin >= (geo.w*4)  || bbox.yMax <= 0 || bbox.yMin >= (geo.h*4) ) {
+		    printf("TxtLayer::glyph 'out of the box!'\n");
+		    continue;
+	       }
+	       /* convert glyph image to bitmap (destroy the glyph) */
+	       ret = FT_Glyph_To_Bitmap( &image, ft_render_mode_normal, 0, 1);
+	       if (!ret) {
+		    FT_BitmapGlyph  bit = (FT_BitmapGlyph)image;
+		    draw_character( bit, bit->left,bit->top,(Uint8 *)buf);
+		    FT_Done_Glyph( image );
+	       }
+	  }
+	  if(blinking) {
+	       word_ff(1);
+	       clear_screen=true;
+	  }
+	  else 
+	       change_word=false;
+     }
+     /* now guess */
+     else if(clear_screen) {
+	  bzero( buf, geo.size);
+	  clear_screen=false;
+     }
+     return buf;
 }
 
 void TxtLayer::close() {
-  func("TxtLayer::close()");
-  FT_Done_FreeType( library );
-  jfree(buf);
-  ::close(fd);
+     func("TxtLayer::close()");
+
+     /* The library doesn't keeps a list of all allocated glyph objects */
+     FT_Done_Face(face);
+     FT_Done_FreeType( library );
+     jfree(buf);
+     ::close(fd);
 }
 
 int TxtLayer::word_ff(int pos) {
-  int c;
-  for(c=pos ; c>0 ; c--) {
-
-    /* if end of line is reached then suck more */
-    if(punt-line >= line_len) {
-      line_len = read(fd,line,512);
-      func("succhiato %i",line_len);
-      /* if nothing more to read, seek to the beginning */
-      if(line_len <= 0) {
-	lseek(fd,0,SEEK_SET);
-	line_len = read(fd,line,512);
-      }
-      line[line_len] = '\0';
-      func("read new line: %s",line);
-      punt = line;
-    }
-    
-    /* go forward until it meets a word */
-    while( (punt-line) < line_len 
-	   && !isgraph(*punt) ) 
-      punt++;
-
-    word = punt;
-
-    /* reaches the end of that word */
-    while(    (punt-line) < line_len
-	      && isgraph(*punt) )
-	      /*	      && *punt != ' '
+     int c;
+     for(c=pos ; c>0 ; c--) {
+	  /* if end of line is reached then suck more */
+	  if(punt-line >= line_len) {
+	       line_len = read(fd,line,512);
+	       func("succhiato %i",line_len);
+	       /* if nothing more to read, seek to the beginning */
+	       if(line_len <= 0) {
+		    lseek(fd,0,SEEK_SET);
+		    line_len = read(fd,line,512);
+	       }
+	       line[line_len] = '\0';
+	       func("read new line: %s",line);
+	       punt = line;
+	  }
+	  /* go forward until it meets a word */
+	  else {
+	       while(!isgraph(*punt)) 
+		    punt++;
+	  }
+	  word = punt;
+	  /* reaches the end of that word */
+	  while(    (punt-line) < line_len
+		    && isgraph(*punt) )
+	       /*	      && *punt != ' '
 			      && *punt != '\0'
 			      && *punt != '\n'
 			      && *punt != '\r'
 			      && *punt != '\t') */
-      punt++;
-    *punt = '\0';
-  }
-  func("word_ff(%i) got \"%s\" string of %i chars",
-       pos, word, punt-word);
-  return(punt-word);
+	       punt++;
+	  *punt = '\0';
+     }
+     func("word_ff(%i) got \"%s\" string of %i chars",
+	       pos, word, punt-word);
+     return(punt-word);
 }
 
 bool TxtLayer::keypress(SDL_keysym *keysym) {
-  int res = 1;
-  switch(keysym->sym) {
-  case SDLK_RIGHT: 
-    change_word=true;
-    clear_screen=true;
-    break;
+     int res = 1;
+     switch(keysym->sym) {
+	  case SDLK_RIGHT: 
+	       change_word=true;
+	       clear_screen=true;
+	       break;
 
-  case SDLK_LEFT:
-    change_word=true;
-    clear_screen=true;
-    word_ff(1);
-    break;
-    
-  default: 
-    res = 0; 
-    break;
-  }
+	  case SDLK_LEFT:
+	       change_word=true;
+	       clear_screen=true;
+	       word_ff(1);
+	       break;
 
-  return res;
+	  case SDLK_b: 
+	       if(!blinking) {
+		    change_word=true;
+		    blinking=true;
+		    word_ff(1);
+	       }
+	       else
+		    blinking=false;
+	       break;
+
+	  case SDLK_l: /* move left */
+	       x-=(geo.bpp/8);
+	       break;
+
+	  case SDLK_r: /* move right */
+	       x+=(geo.bpp/8);
+	       break;
+
+	  case SDLK_d: /* move down */ 
+	       y+=(geo.bpp/8);
+	       break;
+
+	  case SDLK_u: /* move up */
+	       y-=(geo.bpp/8);
+	       break;
+
+	  //case SDLK_w: /* wider */ FIXME cod1o
+	//      set_character_size(text_dimension++);
+	    //   break;
+
+	  //case SDLK_s: /* smaller */
+	    //   set_character_size(text_dimension--);
+	      // break;
+
+	  default: 
+	       res = 0; 
+	       break;
+     }
+
+     return res;
+}
+void TxtLayer::compute_string_bbox( FT_BBox  *abbox,FT_Glyph image ) {
+     FT_BBox  bbox;
+
+     bbox.xMin = bbox.yMin =  32000;
+     bbox.xMax = bbox.yMax = -32000;
+
+     for ( int t = 0; t < num_glyphs; t++ ) {
+	  FT_BBox   glyph_bbox;
+
+	  FT_Glyph_Get_CBox( glyphs[t].image, ft_glyph_bbox_gridfit, &glyph_bbox );
+
+	  if (glyph_bbox.xMin < bbox.xMin)
+	       bbox.xMin = glyph_bbox.xMin;
+
+	  if (glyph_bbox.yMin < bbox.yMin)
+	       bbox.yMin = glyph_bbox.yMin;
+
+	  if (glyph_bbox.xMax > bbox.xMax)
+	       bbox.xMax = glyph_bbox.xMax;
+
+	  if (glyph_bbox.yMax > bbox.yMax)
+	       bbox.yMax = glyph_bbox.yMax;
+     }
+
+     if ( bbox.xMin > bbox.xMax ) {
+	  bbox.xMin = 0;
+	  bbox.yMin = 0;
+	  bbox.xMax = 0;
+	  bbox.yMax = 0;
+     }
+
+     *abbox = bbox;
+}
+bool TxtLayer::draw_character(FT_BitmapGlyph bitmap, int left_side_bearing, int top_side_bearing,Uint8 *dst) {
+     FT_Bitmap pixel_image=bitmap->bitmap;
+     Uint8 *src = bitmap->bitmap.buffer ;
+     dst= (Uint8 *) buf;
+     dst += bitmap->left + (geo.h - bitmap->top)*geo.pitch;
+
+     //printf("top_side_bearing: %d\tleft_side_bearing: %d \n",top_side_bearing,left_side_bearing);
+     /* Second inner loop: draw a letter;
+	they come around but they never come close to */
+     for(int z=0 ; z<(pixel_image.rows) ; z++) {
+
+	  for(int d = pixel_image.pitch; d>0 ; d--) {
+	       //	  *dst++ = *src;
+	       *dst++ = *src++;
+	       // dst[1] = *src;
+	       // dst[2] = *src;
+	       // dst[3] = (alpha_blit) ? 0x00 : 0xff;
+	  }
+	  dst += geo.pitch - pixel_image.pitch;
+     }
+     return(true);
+}
+bool TxtLayer::set_character_size(int _text_dimension) {
+     int ret = FT_Set_Char_Size( face, 0, _text_dimension*64, geo.w, geo.h );
+     if(ret<0) {
+	  error("TxtLayer::Couldn't set character size");
+	  return(false);
+     }
+     return(true);
 }
 
