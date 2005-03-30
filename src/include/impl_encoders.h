bc70f7b01612d1adb8b8cae3ade0964182557d43
kysucix <kysucix@383723c8-4afa-0310-b8a8-b1afb83214fc>
2005-03-30 14:24:40 +0000

    files lost.
    git-svn-id: svn://dyne.org/rastasoft/freej/freej@269 383723c8-4afa-0310-b8a8-b1afb83214fc

---
+++ src/include/impl_encoders.h
@@ -0,0 +1,34 @@
+/*  FreeJ
+ *  (c) Copyright 2005 Silvano Galliani aka kysucix <kysucix@dyne.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * "$Id$"
+ *
+ */
+
+#ifndef __IMPL_ENCODERS_H__
+#define __IMPL_ENCODERS_H__
+
+#include <config.h>
+#include <encoder.h>
+#ifdef WITH_AVCODEC
+#include <ffmpeg_encoder.h>
+#endif
+#include <oggtheora_encoder.h>
+
+
+Encoder *get_encoder(char *output_filename);
+#endif
