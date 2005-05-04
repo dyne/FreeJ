7361a9882847fc5711f79d81c9d147f863b21a56
kysucix <kysucix@383723c8-4afa-0310-b8a8-b1afb83214fc>
2005-05-04 13:33:30 +0000

    common file lost :)
    git-svn-id: svn://dyne.org/rastasoft/freej/freej@275 383723c8-4afa-0310-b8a8-b1afb83214fc

---
+++ src/impl_video_encoders.cpp
@@ -0,0 +1,41 @@
+/*  FreeJ
+ *  (c) Copyright 2001 Silvano Galliani aka kysucix <kysucix@dyne.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * "$Id$"
+ *
+ */
+
+#include <config.h>
+
+#include <video_encoder.h>
+#include <jutils.h>
+
+#include <impl_video_encoders.h>
+
+VideoEncoder *get_encoder(char *output_filename) {
+	VideoEncoder *e = NULL;
+	/*
+	if (strncasecmp( strrchr(filename,'.'), ".ogg",4)) {
+		return (new OggTheoraEncoder());
+		else
+			return (new FFmpegEncoder());
+	*/
+#ifdef WITH_AVCODEC // TODO let select ffmpeg encoder
+	e = new OggTheoraEncoder(output_filename);
+#endif
+	return e;
+}
