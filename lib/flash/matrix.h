14bc2062d51e536592a8beb855f334b97352a2c4
jaromil <jaromil@383723c8-4afa-0310-b8a8-b1afb83214fc>
2006-12-05 21:00:03 +0000

    freej fix http://lists.dyne.org/freej/msg00265.php
    git-svn-id: svn://dyne.org/rastasoft/freej/trunk@805 383723c8-4afa-0310-b8a8-b1afb83214fc

--- lib/flash/matrix.h
+++ lib/flash/matrix.h
@@ -33,13 +33,13 @@ public:
 #endif
 
 	inline
-	long Matrix::getX(long x, long y)
+	long getX(long x, long y)
 	{
 		return (long) (x*a+y*b+tx);
 	};
 
 	inline
-	long Matrix::getY(long x, long y)
+	long getY(long x, long y)
 	{
 		return (long) (x*c+y*d+ty);
 	};
