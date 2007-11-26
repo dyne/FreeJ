0e71a546393d42e2a12cba25b99757da04b32637
jaromil <jaromil@383723c8-4afa-0310-b8a8-b1afb83214fc>
2007-11-26 17:11:46 +0000

    freej: documentation update from 0.9.1 release
    git-svn-id: svn://dyne.org/rastasoft/freej/trunk@1004 383723c8-4afa-0310-b8a8-b1afb83214fc

--- scripts/stream_test_rand.js
+++ scripts/stream_test_rand.js
@@ -99,11 +99,11 @@ register_controller( kbd );
 encoder = new VideoEncoder(10,             128000,        4,             32000);
 
 /*
-encoder.stream_host("giss.tv");
+encoder.stream_host("10.66.66.133");
 encoder.stream_port(8000);
 encoder.stream_title("testing new freej");
 encoder.stream_username("source");
-encoder.stream_password("2t645");
+encoder.stream_password("Revolution");
 encoder.stream_mountpoint("freej-test.ogg");
 */
 
