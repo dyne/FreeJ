b6cfd26b080ec515fa7b357d82ed38913e9f8aaa
mrgoil <mrgoil@383723c8-4afa-0310-b8a8-b1afb83214fc>
2008-03-01 20:39:55 +0000

    vimo test script
    git-svn-id: svn://dyne.org/rastasoft/freej/branches/Goil/freej_91@1099 383723c8-4afa-0310-b8a8-b1afb83214fc

---
+++ scripts/vimo.js
@@ -0,0 +1,24 @@
+// video mouse test script
+
+echo("VideoMouse");
+vm = new ViMoController("/dev/ttyUSB0");
+register_controller(vm);
+
+// inner wheel
+vm.wheel_i = function(a,b,c) {
+	var msg = "mh"+a+" "+b+" "+c+"/";
+	for (i=0; i<arguments.length; i++) {
+		msg += " i:"+i+" = "+arguments[i];
+	}
+	echo(msg);
+}
+
+// outer wheel
+vm.wheel_o = function(s, so) { 
+	echo("speed: " + s + " / " + so);
+}
+
+// ye butts
+vm.button = function(button, state, mask, mask_old) {
+	echo("VM buttom: " + button + " " + state + " " + mask + "/" + mask_old);
+}
