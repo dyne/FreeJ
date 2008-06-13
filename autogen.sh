764902458d18a29e03f31f659813e0de453f43f1
Guido Trotter <ultrotter@quaqua.net>
2008-06-13 09:27:17 +0000

    Don't run configure in autogen.sh
    We want to generate (and check in) autotools related files, not to
    build-depend on the autotools, but configure should not be called, as we
    want it to be build-time dependent.

--- autogen.sh
+++ autogen.sh
@@ -56,10 +56,10 @@ if test "$DIE" -eq 1; then
         exit 1
 fi
 
-if test -z "$*"; then
-        echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
+#if test -z "$*"; then
+#        echo "I am going to run ./configure with no arguments - if you wish "
+#        echo "to pass any to it, please specify them on the $0 command line."
+#fi
 
 echo "Generating configuration files for $package, please wait...."
 
@@ -75,4 +75,4 @@ echo "  $AUTOCONF"
 $AUTOCONF || exit -1
 
 cd $olddir
-$srcdir/configure "$@" && echo
+#$srcdir/configure "$@" && echo
