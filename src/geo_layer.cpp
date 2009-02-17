75b5bd45971dab8ebbde7039ac2bff15cc682c15
Luca Bigliardi <shammash@artha.org>
2009-02-17 15:17:46 +0100

    Guard use of GeoLayer methods.
    Avoid running methods and feed() if layer is not initialized.

--- src/geo_layer.cpp
+++ src/geo_layer.cpp
@@ -83,90 +83,149 @@ void *GeoLayer::feed() {
      or a locking system
      or a queue of operations
    */
-  return(surf->pixels);
+  if (!surf)
+  	return NULL;
+  else
+  	return surf->pixels;
 }
 
 int GeoLayer::clear() {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = SDL_FillRect(surf,NULL,color);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::pixel(int16_t x, int16_t y, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = pixelColor(surf, x, y, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::hline(int16_t x1, int16_t x2, int16_t y, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = hlineColor(surf, x1, x2, y, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::vline(int16_t x, int16_t y1, int16_t y2, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = vlineColor(surf, x, y1, y2, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::rectangle(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = rectangleColor(surf, x1, y1, x2, y2, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::rectangle_fill(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = boxColor(surf, x1, y1, x2, y2, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = lineColor(surf, x1, y1, x2, y2, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::aaline(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = aalineColor(surf, x1, y1, x2, y2, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::circle(int16_t x, int16_t y, int16_t r, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = circleColor(surf, x, y, r, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::aacircle(int16_t x, int16_t y, int16_t r, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = aacircleColor(surf, x, y, r, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::circle_fill(int16_t x, int16_t y, int16_t r, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = filledCircleColor(surf, x, y, r, col);
-  if(res<0) error("error in %s (%i, %i, %i, %u)",__FUNCTION__,x,y,r,col);
+  if(res<0) error("error in %s (%i, %i, %i, %u)",__PRETTY_FUNCTION__,x,y,r,col);
   return(res);
 }
 
 int GeoLayer::ellipse(int16_t x, int16_t y, int16_t rx, int16_t ry, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = ellipseColor(surf, x, y, rx, ry, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::aaellipse(int16_t x, int16_t y, int16_t rx, int16_t ry, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = aaellipseColor(surf, x, y, rx, ry, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::ellipse_fill(int16_t x, int16_t y, int16_t rx, int16_t ry, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = filledEllipseColor(surf, x, y, rx, ry, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
@@ -174,15 +233,23 @@ int GeoLayer::ellipse_fill(int16_t x, int16_t y, int16_t rx, int16_t ry, uint32_
 
 int GeoLayer::pie(uint16_t x, uint16_t y, uint16_t rad,
 		  uint16_t start, uint16_t end, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = pieColor(surf, x, y, rad, start, end, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::pie_fill(uint16_t x, uint16_t y, uint16_t rad,
 		       uint16_t start, uint16_t end, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = filledPieColor(surf, x, y, rad, start, end, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
@@ -191,49 +258,77 @@ int GeoLayer::pie_fill(uint16_t x, uint16_t y, uint16_t rad,
 int GeoLayer::trigon(int16_t x1, int16_t y1,
 	   int16_t x2, int16_t y2,
 	   int16_t x3, int16_t y3, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = trigonColor(surf, x1, y1, x2, y2, x3, y3, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::aatrigon(int16_t x1, int16_t y1,
 	     int16_t x2, int16_t y2,
 	     int16_t x3, int16_t y3, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = aatrigonColor(surf, x1, y1, x2, y2, x3, y3, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::trigon_fill(int16_t x1, int16_t y1,
 		int16_t x2, int16_t y2,
 		int16_t x3, int16_t y3, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = filledTrigonColor(surf, x1, y1, x2, y2, x3, y3, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 ////
 
 int GeoLayer::polygon(int16_t *vx, int16_t *vy, int num_vertex, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = polygonColor(surf, vx, vy, num_vertex, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::aapolygon(int16_t *vx, int16_t *vy, int num_vertex, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = aapolygonColor(surf, vx, vy, num_vertex, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::polygon_fill(int16_t *vx, int16_t *vy, int num_vertex, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = filledPolygonColor(surf, vx, vy, num_vertex, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
 
 int GeoLayer::bezier(int16_t *vx, int16_t *vy, int num_vertex, int steps, uint32_t col) {
+  if(!surf) {
+    error("%s can't run: layer not initialized", __PRETTY_FUNCTION__);
+    return -1;
+  }
   res = bezierColor(surf, vx, vy, num_vertex, steps, col);
-  if(res<0) error("error in %s",__FUNCTION__);
+  if(res<0) error("error in %s",__PRETTY_FUNCTION__);
   return(res);
 }
