673a9364c8bd57a3938a01439984a65f2ef3ff46
Alex Norman <x37v.alex@gmail.com>
2009-06-18 14:51:02 -0700

    removed ViewPort:: from rem_layer in the class definition of ViewPort as it is not needed and causes an error

--- src/include/screen.h
+++ src/include/screen.h
@@ -100,7 +100,7 @@ class ViewPort : public Entry {
   void blit_layers();
 
   virtual bool add_layer(Layer *lay); ///< add a new layer to the screen
-  virtual void ViewPort::rem_layer(Layer *lay);
+  virtual void rem_layer(Layer *lay);
     
   Linklist<Layer> layers; ///< linked list of registered layers
 
