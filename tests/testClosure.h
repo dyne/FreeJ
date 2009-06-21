313fc403e65452fefb7f1e9550444832dbd76855
Luca Bigliardi <shammash@artha.org>
2009-06-21 14:47:45 +0100

    Test framework.
    Add CxxTest files.
    Compile and run tests with "make check" from automake.
    A dumb test is provided, serious tests are coming.

---
+++ tests/testClosure.h
@@ -0,0 +1,17 @@
+#include <cxxtest/TestSuite.h>
+
+class TestClosure : public CxxTest::TestSuite 
+{
+public:
+   void testAddition( void )
+   {
+      TS_ASSERT( 1 + 1 > 1 );
+      TS_ASSERT_EQUALS( 1 + 1, 2 );
+   }
+
+   void testMultiplication( void )
+   {
+      TS_ASSERT_EQUALS( 2 * 2, 5 );
+   }
+};
+
