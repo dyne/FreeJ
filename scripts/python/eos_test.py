67494bd55c7f6e5ee90dac237e0d898bc7531d1c
Jaromil <jaromil@dyne.org>
2009-06-26 17:26:45 +0200

    updated eos_test to reflect python api changes

--- scripts/python/eos_test.py
+++ scripts/python/eos_test.py
@@ -6,27 +6,33 @@ class MyCall(freej.DumbCall):
     super(MyCall, self).__init__(*args)
 
   def callback(self):
-    print "detected EOS from python"
+    print "detected EOS of film (python callback)"
 
-
+freej.set_debug(3)
 
 W = 400
 H = 300
 cx = freej.Context()
-cx.init(W,H,0,0)
-cx.clear_all = True
 
-th = threading.Thread(target = cx.start , name = "freej")
-th.start();
+
+scr = freej.SdlScreen( 400, 300)
+
+cx.add_screen( scr )
 
 v = freej.VideoLayer()
-v.init(cx)
-v.open('/home/jaromil/Movies/TheRevolutionWillNotBeTelevisedGilScottHeron.mp4')
-v.fit()
-v.start()
-v.active = True
 
-cx.add_layer(v)
+v.init(cx)
+
+v.open('/home/jaromil/Movies/TheRevolutionWillNotBeTelevisedGilScottHeron.mp4')
+
+v.start()
+
+cx.add_layer( v )
+
+
 
 cb = MyCall()
 v.add_eos_call(cb)
+
+th = threading.Thread(target = cx.start , name = "freej")
+th.start();
