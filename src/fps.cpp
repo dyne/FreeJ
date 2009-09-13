74a3a3ca0c1326d7b742960406e03b89f65aa008
Xant <xant@dyne.org>
2009-09-13 23:20:14 +0200

    fixed an edge case

--- src/fps.cpp
+++ src/fps.cpp
@@ -95,8 +95,10 @@ void FPS::calc() {
 
   wake_ts.tv_sec  = 0;
   wake_ts.tv_nsec = (rate - done.tv_usec)*1000; // set the delay
-  
-  curr_fps = 1000000 /  done.tv_usec;
+  if (done.tv_usec)
+      curr_fps = 1000000 /  done.tv_usec;
+  else
+      curr_fps = 0;
 
   // statistic only
   fpsd.sum = fpsd.sum - fpsd.data[fpsd.i] + curr_fps;
