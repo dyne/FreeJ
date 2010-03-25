8ce4127ed5ad119de6dcf8a973628da7ecb664fb
Robin Gareus <rgareus@priroda.local>
2010-03-25 16:11:33 +0100

    added virtual interface protocol for File-Exporter abstraction.

---
+++ xcode/Exporter.h
@@ -0,0 +1,32 @@
+/*  FreeJ
+ *  (c) Copyright 2010 Robin Gareus <robin@gareus.org>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#ifndef __EXPORTER_H__
+#define __EXPORTER_H__
+
+@class CVScreenView;
+//@class Exporter;
+
+@protocol Exporter 
+- (BOOL)startExport;
+- (void)stopExport;
+- (BOOL)isRunning;
+@end
+
+#endif
