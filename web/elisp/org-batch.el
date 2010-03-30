aa6bc90ea2bbbc08a6917f019f9dc8e89b099778
Jaromil <jaromil@dyne.org>
2010-03-30 18:26:54 +0200

    more fixes to website build

--- web/elisp/org-batch.el
+++ web/elisp/org-batch.el
@@ -1,22 +1,19 @@
-;; emacs batch mode configuration
+;; emacs org-mode batch configuration
 
 ;; local emacs extensions
-(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/")
-(add-to-list 'load-path ".")
+(add-to-list 'load-path "./elisp")
 
 ;;; ORG mode
 (require 'org-install)
 (require 'org-exp-blocks)
-
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-twilight)
-
 (require 'htmlize)
-'(htmlize-ignore-face-size (quote absolute))
-'(htmlize-output-type (quote inline-css))
+
+;; set auto load on .org files
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
 ;; org mode customisations
 (setq org-export-htmlize-output-type 'css)
+
 
 '(org-export-blocks (quote ((comment org-export-blocks-format-comment t) (ditaa org-export-blocks-format-ditaa nil) (dot org-export-blocks-format-dot t) (r org-export-blocks-format-R nil) (R org-export-blocks-format-R nil))))
 '(org-export-html-inline-images t)
@@ -26,5 +23,5 @@
 '(org-export-htmlize-output-type "css")
 '(org-export-html-validation-link "<p class=\"xhtml-validation\"><a href=\"http://validator.w3.org/check?uri=referer\">Validate XHTML 1.0</a></p>")
 '(org-export-html-with-timestamp nil)
-'(org-modules (quote (org-bbdb org-bibtex org-gnus org-info org-jsinfo org-irc org-w3m org-mouse org-eval org-eval-light org-exp-bibtex org-man org-mtags org-panel org-R org-special-blocks org-exp-blocks)))
+'(org-modules (quote (org-bbdb org-bibtex org-info org-jsinfo org-irc org-w3m org-mouse org-eval org-eval-light org-exp-bibtex org-man org-mtags org-panel org-R org-special-blocks org-exp-blocks)))
 
