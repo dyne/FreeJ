b92958d44bc044c92de000ae313b1a32bf42ba40
xant <xant@dyne.org>
2010-05-17 21:05:57 +0200

    empty files (but already included in the project)

---
+++ xcode/CVFilter.cpp
@@ -0,0 +1,27 @@
+/*
+ *  CVFilter.cpp
+ *  freej
+ *
+ *  Created by xant on 5/4/10.
+ *  Copyright 2010 dyne.org. All rights reserved.
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * "$Id:$"
+ *
+ */
+
+#include "CVFilter.h"
+
