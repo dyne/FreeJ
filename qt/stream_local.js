946e863206dd0396ae365fd4b198471cc8b71b4c
fred <fred@home.lan>
2010-11-16 12:07:20 +0100

    just records what freej is playing.
    it only works for me if I comment the line
    encoder.add_audio(audio) .... need to be investigated.

--- qt/stream_local.js
+++ qt/stream_local.js
@@ -11,19 +11,8 @@ encoder = new VideoEncoder(12,        300000,     6,         48000);
 // Add the audio channel in the video encoded
 encoder.add_audio(audio);
 
-// Configure the encoder to stream over an Icecast server
-//encoder.stream_host("toto.com");
-//encoder.stream_port(4567);
-//encoder.stream_title("testing new freej");
-//encoder.stream_username("toto");
-//encoder.stream_password("retoto");
-//encoder.stream_mountpoint("freej.ogg");
-
 // Register the encoder on the running FreeJ engine
 register_encoder(encoder);
-
-// Start a network stream
-//encoder.start_stream();
 
 // Record the stream into a local file
 encoder.start_filesave('Video/freej-test.ogg');
